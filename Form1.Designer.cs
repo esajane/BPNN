ff3d439b10d154e84ba3283d2df4698dd04ca365
aexizn <janessa.pandiyan@cit.edu>
2023-12-21 13:59:29 +0800

    Commiting Murder

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace BackPropagation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstInput = new System.Windows.Forms.TextBox();
+            this.secondInput = new System.Windows.Forms.TextBox();
+            this.thirdInput = new System.Windows.Forms.TextBox();
+            this.fourthInput = new System.Windows.Forms.TextBox();
+            this.oneOutput = new System.Windows.Forms.TextBox();
+            this.createBPNN = new System.Windows.Forms.Button();
+            this.trainNN = new System.Windows.Forms.Button();
+            this.Test = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calMin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstInput
+            // 
+            this.firstInput.Location = new System.Drawing.Point(38, 81);
+            this.firstInput.Name = "firstInput";
+            this.firstInput.Size = new System.Drawing.Size(100, 20);
+            this.firstInput.TabIndex = 0;
+            this.firstInput.Text = "First Input";
+            // 
+            // secondInput
+            // 
+            this.secondInput.Location = new System.Drawing.Point(38, 130);
+            this.secondInput.Name = "secondInput";
+            this.secondInput.Size = new System.Drawing.Size(100, 20);
+            this.secondInput.TabIndex = 1;
+            this.secondInput.Text = "Second Input";
+            // 
+            // thirdInput
+            // 
+            this.thirdInput.Location = new System.Drawing.Point(38, 176);
+            this.thirdInput.Name = "thirdInput";
+            this.thirdInput.Size = new System.Drawing.Size(100, 20);
+            this.thirdInput.TabIndex = 2;
+            this.thirdInput.Text = "Third Input";
+            // 
+            // fourthInput
+            // 
+            this.fourthInput.Location = new System.Drawing.Point(38, 224);
+            this.fourthInput.Name = "fourthInput";
+            this.fourthInput.Size = new System.Drawing.Size(100, 20);
+            this.fourthInput.TabIndex = 3;
+            this.fourthInput.Text = "Fourth Input";
+            // 
+            // oneOutput
+            // 
+            this.oneOutput.Location = new System.Drawing.Point(309, 161);
+            this.oneOutput.Name = "oneOutput";
+            this.oneOutput.Size = new System.Drawing.Size(100, 20);
+            this.oneOutput.TabIndex = 4;
+            this.oneOutput.Text = "Ouput";
+            // 
+            // createBPNN
+            // 
+            this.createBPNN.Location = new System.Drawing.Point(105, 290);
+            this.createBPNN.Name = "createBPNN";
+            this.createBPNN.Size = new System.Drawing.Size(75, 23);
+            this.createBPNN.TabIndex = 5;
+            this.createBPNN.Text = "createBPNN";
+            this.createBPNN.UseVisualStyleBackColor = true;
+            this.createBPNN.Click += new System.EventHandler(this.createBPNN_Click);
+            // 
+            // trainNN
+            // 
+            this.trainNN.Location = new System.Drawing.Point(264, 290);
+            this.trainNN.Name = "trainNN";
+            this.trainNN.Size = new System.Drawing.Size(75, 23);
+            this.trainNN.TabIndex = 6;
+            this.trainNN.Text = "Train NN";
+            this.trainNN.UseVisualStyleBackColor = true;
+            this.trainNN.Click += new System.EventHandler(this.trainNN_Click);
+            // 
+            // Test
+            // 
+            this.Test.Location = new System.Drawing.Point(438, 290);
+            this.Test.Name = "Test";
+            this.Test.Size = new System.Drawing.Size(75, 23);
+            this.Test.TabIndex = 7;
+            this.Test.Text = "Test";
+            this.Test.UseVisualStyleBackColor = true;
+            this.Test.Click += new System.EventHandler(this.Test_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(689, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(689, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "?";
+            // 
+            // calMin
+            // 
+            this.calMin.Location = new System.Drawing.Point(627, 290);
+            this.calMin.Name = "calMin";
+            this.calMin.Size = new System.Drawing.Size(75, 23);
+            this.calMin.TabIndex = 11;
+            this.calMin.Text = "Print Minimum";
+            this.calMin.UseVisualStyleBackColor = true;
+            this.calMin.Click += new System.EventHandler(this.calMin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(504, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Minimum number of hidden neurons :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(503, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Minimum number of training epochs :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.calMin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Test);
+            this.Controls.Add(this.trainNN);
+            this.Controls.Add(this.createBPNN);
+            this.Controls.Add(this.oneOutput);
+            this.Controls.Add(this.fourthInput);
+            this.Controls.Add(this.thirdInput);
+            this.Controls.Add(this.secondInput);
+            this.Controls.Add(this.firstInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstInput;
+        private System.Windows.Forms.TextBox secondInput;
+        private System.Windows.Forms.TextBox thirdInput;
+        private System.Windows.Forms.TextBox fourthInput;
+        private System.Windows.Forms.TextBox oneOutput;
+        private System.Windows.Forms.Button createBPNN;
+        private System.Windows.Forms.Button trainNN;
+        private System.Windows.Forms.Button Test;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calMin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Backprop; 
+
+namespace BackPropagation
+{
+    public partial class Form1 : Form
+    {
+        private NeuralNet neuralNetwork;
+        private List<TrainingData> trainingData;
+
+        public Form1()
+        {
+            InitializeComponent();
+            trainingData = GenerateANDTrainingData();
+        }
+
+        private void createBPNN_Click(object sender, EventArgs e)
+        {
+            Tuple<int, int> optimalSettings = FindOptimalSettings();
+            neuralNetwork = new NeuralNet(4, optimalSettings.Item1, 1);
+            MessageBox.Show($"Neural Network Initialized with {optimalSettings.Item1} hidden neurons.");
+        }
+
+        private void trainNN_Click(object sender, EventArgs e)
+        {
+            Tuple<int, int> optimalSettings = FindOptimalSettings();
+            TrainNetwork(optimalSettings.Item2);
+            MessageBox.Show("Training Completed with " + optimalSettings.Item2 + " epochs.");
+        }
+
+        private void Test_Click(object sender, EventArgs e)
+        {
+            TestNetwork();
+        }
+
+        private List<TrainingData> GenerateANDTrainingData()
+        {
+            var trainingData = new List<TrainingData>();
+            for (int i = 0; i < 16; i++)
+            {
+                var inputs = Convert.ToString(i, 2).PadLeft(4, '0')
+                    .Select(c => Convert.ToDouble(c.ToString())).ToArray();
+                var output = inputs.All(input => input == 1) ? 1 : 0;
+                trainingData.Add(new TrainingData(inputs, output));
+            }
+            return trainingData;
+        }
+
+        private void TrainNetwork(int epochs)
+        {
+            foreach (var data in trainingData)
+            {
+                for (int i = 0; i < data.Inputs.Length; i++)
+                {
+                    neuralNetwork.setInputs(i, data.Inputs[i]);
+                }
+                neuralNetwork.setDesiredOutput(0, data.Output);
+
+                for (int epoch = 0; epoch < epochs; epoch++)
+                {
+                    neuralNetwork.learn();
+                }
+            }
+        }
+
+        private void TestNetwork()
+        {
+            try
+            {
+                double[] inputs = new double[] {
+                    Convert.ToDouble(firstInput.Text),
+                    Convert.ToDouble(secondInput.Text),
+                    Convert.ToDouble(thirdInput.Text),
+                    Convert.ToDouble(fourthInput.Text)
+                };
+
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    neuralNetwork.setInputs(i, inputs[i]);
+                }
+
+                neuralNetwork.run();
+                oneOutput.Text = neuralNetwork.getOuputData(0).ToString();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Invalid input format: " + ex.Message);
+            }
+        }
+
+        private Tuple<int, int> FindOptimalSettings()
+        {
+            int minHiddenNeurons = 0; 
+            int maxHiddenNeurons = 10; 
+            int minEpochs = 0; 
+            int maxEpochs = 100; 
+            double bestError = double.MaxValue;
+            double acceptableError = 0.01; 
+
+            for (int hiddenNeurons = minHiddenNeurons; hiddenNeurons <= maxHiddenNeurons; hiddenNeurons++)
+            {
+                for (int epochs = minEpochs; epochs <= maxEpochs; epochs += 10) 
+                {
+                    neuralNetwork = new NeuralNet(4, hiddenNeurons, 1);
+                    double error = TrainAndEvaluate(epochs);
+
+                    if (error < bestError)
+                    {
+                        bestError = error;
+                        minHiddenNeurons = hiddenNeurons;
+                        minEpochs = epochs;
+                    }
+
+                    if (error <= acceptableError)
+                    {
+                        return new Tuple<int, int>(hiddenNeurons, epochs);
+                    }
+                }
+            }
+
+            return new Tuple<int, int>(minHiddenNeurons, minEpochs);
+        }
+
+
+        private double TrainAndEvaluate(int epochs)
+        {
+            TrainNetwork(epochs);
+            return CalculateMeanSquaredError();
+        }
+
+        private double CalculateMeanSquaredError()
+        {
+            double totalError = 0;
+            foreach (var data in trainingData)
+            {
+                for (int i = 0; i < data.Inputs.Length; i++)
+                {
+                    neuralNetwork.setInputs(i, data.Inputs[i]);
+                }
+                neuralNetwork.run();
+                double error = Math.Pow(data.Output - neuralNetwork.getOuputData(0), 2);
+                totalError += error;
+            }
+            return totalError / trainingData.Count;
+        }
+
+        private void calMin_Click(object sender, EventArgs e)
+        {
+            Tuple<int, int> optimalSettings = FindOptimalSettings();
+            label1.Text = "Min Hidden Neurons: " + optimalSettings.Item1;
+            label2.Text = "Min Training Epochs: " + optimalSettings.Item2;
+        }
+
+        private class TrainingData
+        {
+            public double[] Inputs { get; }
+            public double Output { get; }
+
+            public TrainingData(double[] inputs, double output)
+            {
+                Inputs = inputs;
+                Output = output;
+            }
+        }
+
+    }
+}
